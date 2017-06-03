8bc72ee870a23994a88919a22379edfca72c1d77
T-ashby <jacobobbema@gmail.com>
2017-06-03 12:22:39 -0400

    Created array containing all players and their dictionaries

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -170,3 +170,14 @@ let player18: [String: String] = [
     
 ]
 
+// Array storing all data for players
+
+var players = [
+    player1, player2, player3, player4, player5,
+    player6, player7, player8, player9, player10,
+    player11, player12, player13, player14,
+    player15, player16, player17, player18
+]
+
+
+
