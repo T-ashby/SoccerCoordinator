2e9975361462f255f72f68a94bd63ab2eafd48b7
T-ashby <jacobobbema@gmail.com>
2017-06-03 12:19:51 -0400

    Created dictionaries for each player

--- SoccerCoordinator.playground/Contents.swift
+++ SoccerCoordinator.playground/Contents.swift
@@ -1,4 +1,172 @@
 
 //Thomas's comment
 
+/*
+ 
+ Created dictionaries for each player for storage
+ in an array for all player info
+ 
+*/
+
+let player1: [String: String] = [
+    
+    "name": "Joe Smith" ,
+    "height": "42" ,
+    "experience": "Yes" ,
+    "guardians": "Jim and Jan Smith"
+    
+]
+
+let player2: [String: String] = [
+    
+    "name": "Jill Tanner" ,
+    "height": "36" ,
+    "experience": "Yes" ,
+    "guardians": "Clara Tanner"
+    
+]
+
+let player3: [String: String] = [
+    
+    "name": "Bill Bon" ,
+    "height": "43" ,
+    "experience": "Yes" ,
+    "guardians": "Sara and Jenny Bon"
+    
+]
+
+let player4: [String: String] = [
+    
+    "name": "Eva Gordon" ,
+    "height": "45" ,
+    "experience": "No" ,
+    "guardians": "Wendey and Mike Gordon"
+    
+]
+
+let player5: [String: String] = [
+    
+    "name": "Matt Gill" ,
+    "height": "40" ,
+    "experience": "No" ,
+    "guardians": "Charles and Sylvia Gill"
+    
+]
+
+let player6: [String: String] = [
+    
+    "name": "Kimmy Stein" ,
+    "height": "41" ,
+    "experience": "No" ,
+    "guardians": "Bill and Hillary Stein"
+    
+]
+
+let player7: [String: String] = [
+    
+    "name": "Sammy Adams" ,
+    "height": "45" ,
+    "experience": "No" ,
+    "guardians": "Jeff Adams"
+    
+]
+
+let player8: [String: String] = [
+    
+    "name": "Karl Saygan" ,
+    "height": "42" ,
+    "experience": "Yes" ,
+    "guardians": "Heather Bledsoe"
+    
+]
+
+let player9: [String: String] = [
+    
+    "name": "Suzane Greenburg" ,
+    "height": "44" ,
+    "experience": "Yes" ,
+    "guardians": "Henrietta Dumas"
+    
+]
+
+let player10: [String: String] = [
+    
+    "name": "Sal Dali" ,
+    "height": "41" ,
+    "experience": "No" ,
+    "guardians": "Gala Dali"
+    
+]
+
+let player11: [String: String] = [
+    
+    "name": "Joe Kavalier" ,
+    "height": "39" ,
+    "experience": "No" ,
+    "guardians": "Sam and Elaine Kavalier"
+    
+]
+
+let player12: [String: String] = [
+    
+    "name": "Ben Finklestein" ,
+    "height": "44" ,
+    "experience": "No" ,
+    "guardians": "Aaron and Jill Finklestein"
+    
+]
+
+let player13: [String: String] = [
+    
+    "name": "Diego Soto" ,
+    "height": "41" ,
+    "experience": "Yes" ,
+    "guardians": "Robin and Sarika Soto"
+    
+]
+
+let player14: [String: String] = [
+    
+    "name": "Chloe Alaska" ,
+    "height": "47" ,
+    "experience": "No" ,
+    "guardians": "David and Jamie Alaska"
+    
+]
+
+let player15: [String: String] = [
+    
+    "name": "Arnold Willis" ,
+    "height": "43" ,
+    "experience": "No" ,
+    "guardians": "Claire Willis"
+    
+]
+
+let player16: [String: String] = [
+    
+    "name": "Phillip Helm" ,
+    "height": "44" ,
+    "experience": "Yes" ,
+    "guardians": "Thomas Helm and Eva Jones"
+    
+]
+
+let player17: [String: String] = [
+    
+    "name": "Les Clay" ,
+    "height": "42" ,
+    "experience": "Yes" ,
+    "guardians": "Wynonna Brown"
+    
+]
+
+let player18: [String: String] = [
+    
+    "name": "Herschel Krustofski" ,
+    "height": "45" ,
+    "experience": "Yes" ,
+    "guardians": "Hyman and Rachel Krustofski"
+    
+]
 
